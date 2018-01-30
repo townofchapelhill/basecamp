7ef234bed1fbaed895701209a47f9d3685db6122
Steven Nguyen <stevenan@live.unc.edu>
2018-01-30 14:57:15 -0500

    updated permits and transit blocks

--- columntest.js
+++ columntest.js
@@ -15,34 +15,6 @@ function min_to_ms(min) {
     return min*60*1000;
 }
 
-// update open data block
-function update_od_transit_police() {
-    $.getJSON("https://www.chapelhillopendata.org/api/datasets/1.0/search?rows=1&apikey=" + ODS_api + "&callback=?", function(datasets){
-        // set number of datasets
-        $('#numD').text(datasets.nhits);
-    });
-    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/analyze/?dataset=ods-datasets-monitoring&source=monitoring&y.total_records.expr=records_count&y.total_records.func=SUM&apikey=" + ODS_api + "&callback=?", function(records){
-        // set number of records total
-        $('#numR').text(records[0].total_records);
-    });
-    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/analyze/?dataset=ods-api-monitoring&q=user_id:anonymous&source=monitoring&x=user_ip_addr&y.serie1.func=COUNT&apikey=" + ODS_api + "&callback=?", function(anon_users){
-        $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/analyze/?dataset=ods-api-monitoring&q=not+user_id:anonymous&source=monitoring&x=user_id&y.serie1.func=COUNT&apikey="  + ODS_api + "&callback=?", function(users){
-        // set number of users total
-        $('#numU').text(anon_users.length + users.length);
-        });
-        
-        // update transit info
-        var active_routes = [2, 24, 24, 24, 27, 27, 11];
-        var in_service = [2, 100, 98, 100, 128, 128, 11];
-        $('#activeR').text(active_routes[currDay]);
-        $('#inService').text(in_service[currDay]);
-        
-        // update police info
-        $('#hours').text(4528.5);
-        $('#calls').text(7553);
-    });
-}
-
 // update permit data & transit data
 function update_permits() {
     $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/download/?dataset=permits&format=json&apikey=" + ODS_api + "&callback=?", function(today){
@@ -59,19 +31,19 @@ function update_permits() {
         // loop through data and increment counter accordingly
         for(var i = 0; i < today.length; i++) {
             try{
-                var date = today[i].fields.issue_date.split('/');
-                var file_date = today[i].fields.date_filed.split('/');
+                var date = today[i].fields.issue_date.split('-');
+                var file_date = today[i].fields.date_filed.split('-');
                 
-                if(date[0] == current_month && date[2] == current_year) {
+                if(date[1] == current_month && date[0] == current_year) {
                     m_count+=1;
                 }
-                if(date[0] == current_month && date[1] == current_day && date[2] == current_year) {
+                if(date[1] == current_month && date[2] == current_day && date[0] == current_year) {
                     d_count+=1;
                 }
-                if(file_date[0] == current_month && file_date[2] == current_year) {
+                if(file_date[1] == current_month && file_date[0] == current_year) {
                     fm_count+=1;
                 }
-                if(file_date[0] == current_month && file_date[1] == current_day && file_date[2] == current_year) {
+                if(file_date[1] == current_month && file_date[2] == current_day && file_date[0] == current_year) {
                     fd_count+=1;
                 }
             }catch(e) {
@@ -86,6 +58,62 @@ function update_permits() {
     });    
 }
 
+// update items info
+function update_items() {
+    var total_checked_out = 0;
+    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=overdue-items&rows=1&apikey=" + ODS_api + "&callback=?", function(od_items) {
+        // add amount of overdue items to checked out items
+        $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=checked-out-items&rows=1&apikey=" + ODS_api + "&callback=?", function(co_items) {
+            total_checked_out = co_items.nhits+od_items.nhits;
+            $('#checkedOut').text(total_checked_out);
+            $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=library-items&rows=1&facet=status&refine.status=AVAILABLE&apikey=" + ODS_api + "&callback=?", function(total_a) {
+                $('#percentOut').text((total_checked_out/total_a.nhits*100).toFixed(2));
+            });
+        });
+    });
+    // add active patrons
+    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=activepatrons&rows=1&apikey=" + ODS_api + "&callback=?", function(ap) {
+        $('#activeP').text(ap.nhits);
+    });
+}
+
+// update open data block
+function update_od_police() {
+    $.getJSON("https://www.chapelhillopendata.org/api/datasets/1.0/search?rows=1&apikey=" + ODS_api + "&callback=?", function(datasets){
+        // set number of datasets
+        $('#numD').text(datasets.nhits);
+    });
+    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/analyze/?dataset=ods-datasets-monitoring&source=monitoring&y.total_records.expr=records_count&y.total_records.func=SUM&apikey=" + ODS_api + "&callback=?", function(records){
+        // set number of records total
+        $('#numR').text(records[0].total_records);
+    });
+    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/analyze/?dataset=ods-api-monitoring&q=user_id:anonymous&source=monitoring&x=user_ip_addr&y.serie1.func=COUNT&apikey=" + ODS_api + "&callback=?", function(anon_users){
+        $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/analyze/?dataset=ods-api-monitoring&q=not+user_id:anonymous&source=monitoring&x=user_id&y.serie1.func=COUNT&apikey="  + ODS_api + "&callback=?", function(users){
+        // set number of users total
+        $('#numU').text(anon_users.length + users.length);
+        });
+        
+        // update police info
+        $('#hours').text(4528.5);
+        $('#calls').text(7553);
+    });
+}
+
+// update transit info
+function update_transit() {
+    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=nextbuscount&rows=1&sort=route&facet=route&apikey=" + ODS_api + "&callback=?", function(active){
+        $('#activeR').text(active.facet_groups[0].facets.length); 
+    });
+    
+    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=nextbuscount&rows=100&sort=route&apikey=" + ODS_api + "&callback=?", function(buses) {
+        var totalSpeed = 0;
+        $('#inService').text(buses.nhits);
+        for(var i = 0; i < buses.records.length; i++){
+            totalSpeed += buses.records[i].fields.speed_km_hr;
+        }
+        $('#avgSpeed').text(totalSpeed/buses.records.length);
+    });
+}
 
 // update website data info
 function update_site_data() {
@@ -112,43 +140,26 @@ function update_site_data() {
     });
 }
 
-
-// update items info
-function update_items() {
-    var total_checked_out = 0;
-    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=overdue-items&rows=1&apikey=" + ODS_api + "&callback=?", function(od_items) {
-        // add amount of overdue items to checked out items
-        $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=checked-out-items&rows=1&apikey=" + ODS_api + "&callback=?", function(co_items) {
-            total_checked_out = co_items.nhits+od_items.nhits;
-            $('#checkedOut').text(total_checked_out);
-            $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=library-items&rows=1&facet=status&refine.status=AVAILABLE&apikey=" + ODS_api + "&callback=?", function(total_a) {
-                $('#percentOut').text((total_checked_out/total_a.nhits*100).toFixed(2));
-            });
-        });
-    });
-    // add active patrons
-    $.getJSON("https://www.chapelhillopendata.org/api/records/1.0/search/?dataset=activepatrons&rows=1&apikey=" + ODS_api + "&callback=?", function(ap) {
-        $('#activeP').text(ap.nhits);
-    });
-}
-
 // function that gets a json and updates the page 
 function update_page() {
     // gets local json file
     /*global $*/
     /*global ODS_api*/
     
-    // update Open Data info
-    update_od_transit_police();
-    
     // update Permit and Transit info
     update_permits();
     
-    // update website info
-    update_site_data();
-    
     // update items info
     update_items();
+    
+    // update Open Data info
+    update_od_police();
+    
+    // update Transit info
+    update_transit();
+    
+    // update website info
+    update_site_data();
     
     // display updated date
     var d = new Date();
