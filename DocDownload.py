0f77db4e317e4dfae6d9f22246d9959b500124b6
Steven Nguyen <stevenan@live.unc.edu>
2018-03-30 11:52:21 -0400

    update - downloads all docs in 'Automation for the People'

--- DocDownload.py
+++ DocDownload.py
@@ -3,19 +3,89 @@ import json
 import datetime
 import secrets
 
-now = datetime.datetime.now()
-
-# print program start timestamp
-print(str(now) + '\n')
-
 # use token and send request to get json response of projects & teams
 token = secrets.basecamp_access
 
-test_js = "https://3.basecampapi.com/3472161/buckets/6505832/uploads/904675583/download/foo.js"
-request = requests.get(test_js, headers={
-    "Authorization": "Bearer " + token,
-    "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
-})
-  
-with open('test.js', 'wb') as f:
-    f.write(request.content)
+# check vault function 
+def check_vault(doc):
+    # check if there are documents in each container
+    print(doc["documents_count"])
+    if doc["documents_count"] != 0:
+        docs_url = doc["documents_url"]
+        docs_request = requests.get(docs_url, headers ={
+            "Authorization": "Bearer " + token,
+            "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+        })
+        docs = json.loads(docs_request.text)
+        
+        for j in range(len(docs)):
+            download = requests.get(docs[j]["downloads_url"], headers={
+                "Authorization": "Bearer " + token,
+                "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+            })
+              
+            with open(docs[j]["filename"], 'wb') as f:
+                f.write(download.content)
+    
+    if doc["uploads_count"] != 0:
+        uploads_url = doc["uploads_url"]
+        upload_request = requests.get(uploads_url, headers ={
+            "Authorization": "Bearer " + token,
+            "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+        })
+        uploads = json.loads(upload_request.text)
+        
+        for k in range(len(uploads)):
+            download = requests.get(uploads[k]["download_url"], headers={
+                "Authorization": "Bearer " + token,
+                "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+            })
+              
+            with open(uploads[k]["filename"], 'wb') as f:
+                f.write(download.content)
+        
+        
+    if doc["vaults_count"] != 0:
+        vaults_url = doc["vaults_url"]
+        vaults_request = requests.get(vaults_url, headers ={
+            "Authorization": "Bearer " + token,
+            "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+        })
+        vaults = json.loads(vaults_request.text)
+        
+        for l in range(len(vaults)):
+            check_vault(vaults[l])
+
+def main():            
+    # get current timestamp
+    now = datetime.datetime.now()
+    
+    # print program start timestamp
+    print(str(now) + '\n')
+    
+    # access list of projects for account associated with token
+    project_url = "https://3.basecampapi.com/3472161/projects.json"
+    request = requests.get(project_url, headers={
+        "Authorization": "Bearer " + token,
+        "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+    })
+    
+    # set json file to variable
+    projects = json.loads(request.text)
+    
+    # loop through projects/teams
+    for i in range(1):
+        # access list of documents for account associated with token in team at index i
+        doc_url = projects[i]["dock"][5]["url"]
+        doc_request = requests.get(doc_url, headers={
+            "Authorization": "Bearer " + token,
+            "user-agent": "Town of Chapel Hill Basecamp Integration (snguyen@townofchapelhill.org)"
+        })
+        
+        # set document json to variable
+        documents = json.loads(doc_request.text)
+        
+        # call function to check vault
+        check_vault(documents)
+
+main()
